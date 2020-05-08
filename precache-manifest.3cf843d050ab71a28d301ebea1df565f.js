1c2a61d0d389eb7e3f3d989716e08004ccf0eeb3
Jinxin Hu <hjxenjoy@gmail.com>
2020-05-08 13:47:45 +0800

    Updates

---
+++ precache-manifest.3cf843d050ab71a28d301ebea1df565f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c1e72cafe7828469c70c7acca97fec",
+    "url": "/drag-and-paste/index.html"
+  },
+  {
+    "revision": "b499146e956faa571ecb",
+    "url": "/drag-and-paste/static/css/main.55848f66.chunk.css"
+  },
+  {
+    "revision": "8ce0d686268801c1dd46",
+    "url": "/drag-and-paste/static/js/2.6be9f16f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drag-and-paste/static/js/2.6be9f16f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b499146e956faa571ecb",
+    "url": "/drag-and-paste/static/js/main.f5251671.chunk.js"
+  },
+  {
+    "revision": "8276be688a87c508a230",
+    "url": "/drag-and-paste/static/js/runtime-main.b9fe0c75.js"
+  }
+]);
